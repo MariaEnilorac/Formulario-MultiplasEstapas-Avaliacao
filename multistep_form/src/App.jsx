57b70109f48a56683a5e5289841b48753d93af14
MariaElinorac <karolfcjc@gmail.com>
2025-02-09 18:42:52 -0300

    att

--- multistep_form/src/App.jsx
+++ multistep_form/src/App.jsx
@@ -4,9 +4,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>aaaaaaaaaaaaa</h1>
-      </div>
+      <div className="app">
+        <div className="header">
+          <h2>Deixe sua avaliação</h2>
+          <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto</p>
+        </div>
+        <div className="form-container">
+          <p>etapas</p>
+          <form>
+            
+          </form>
+        </div>
+        </div>
       
     </>
   )
